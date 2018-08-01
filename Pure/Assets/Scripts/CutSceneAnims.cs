f7201c195c3ceccc54669db6a01643525224196b
Shahbaz Fazal <shahbazfazal@hotmail.co.uk>
2018-08-01 01:29:32 +0100

    added cinemachine cams

---
+++ Pure/Assets/Scripts/CutSceneAnims.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneAnims : MonoBehaviour {
+
+    private Animator EveAnim;
+
+	// Use this for initialization
+	void Start () {
+        EveAnim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Climb")
+        {
+            //EveAnim.StopPlayback();
+            EveAnim.Play("Sprint To Wall Climb");
+        }
+
+        if (other.name == "ClimbOver")
+        {
+            //EveAnim.StopPlayback();
+            EveAnim.Play("Climbing");
+        }
+    }
+}
